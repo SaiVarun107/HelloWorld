fd74e4283eab3c07528b4ed2bd99fdb2a69f5fa8
SaiVarun107 <saivarunbitra107.com>
2025-05-23 21:43:07 +0530

    UC2

--- src/main/java/com/example/demo/controller/control.java
+++ src/main/java/com/example/demo/controller/control.java
@@ -7,9 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class control {
 
+	// UC1-hello
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
     }
-       
+    
+    // UC2-user
+    @GetMapping("/user")
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
+	}
 }
